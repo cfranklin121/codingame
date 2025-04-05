cf939f18ec3f5978642e1327910e278d328f21b9
cfranklin121 <courtenayfranklin@gmail.com>
2025-04-05 14:20:31 -0700

    Added 'Unary'

---
+++ unary/unary.py
@@ -0,0 +1,34 @@
+import sys
+import math
+
+#Input
+message = input()
+binary_message = "" #Create enpty string for the binary message
+
+for char in list(message):
+    binary_message = binary_message + (format(ord(char), "07b"))
+binary_message = binary_message + "x" #Add extra character to avoid an out of range index error
+
+index = [0] #List of every index that stars a different groupd of numbers
+
+for i in range(len(binary_message) - 1):
+    if binary_message[i] != binary_message[i + 1]:
+        index.append(i + 1)
+
+character_count = [] #Number of 0's or 1's in each group
+
+for i in range(len(index) - 1):
+    character_count.append(index[i + 1] - index[i])
+
+#Output
+for i in range(len(index) - 1):
+    if binary_message[index[i]] == "1":
+        print("0", end=" ")
+    if binary_message[index[i]] == "0":
+        print("00", end=" ")
+
+    for x in range(character_count[i]):
+        print("0", end= "")
+    
+    if i < (len(index) - 2):
+        print(end=" ")
