7efe4d72f702b0feea66a32eee993ea90a37a3f1
cfranklin121 <courtenayfranklin@gmail.com>
2025-04-06 19:06:29 -0700

    Version 1: cleaning up bugs

--- you_are_the_father/you_are_the_father.py
+++ you_are_the_father/you_are_the_father.py
@@ -7,7 +7,8 @@ def get_mother_child_chromPairs(person):
 
 def get_father_info(person):
     lst = person.split()
-    return lst [0], " ".join(lst[1:])
+    no_colan = lst[0].split(":")
+    return no_colan[0], " ".join(lst[1:])
 
 mother = input()
 print(mother, type(mother))
@@ -22,8 +23,7 @@ child_chrompairs = get_mother_child_chromPairs(child)
 num_of_possible_fathers = int(input())
 
 check = False
-pair1 = False
-pair2 = False
+
 print(num_of_possible_fathers, type(num_of_possible_fathers))
 for i in range(num_of_possible_fathers):
     
@@ -33,15 +33,15 @@ for i in range(num_of_possible_fathers):
     #print(father_name)
     #print(father_chrompairs)
 
-    pair1 = False
-    pair2 = False
+    
     for pair in child_chrompairs:
         #false = 1st pair. true = 2nd pair
         print(check)
         print(pair[0], pair[1])
         print("father: ", father_chrompairs)
         
-        
+        pair1 = False
+        pair2 = False
         if pair[0] in father_chrompairs or pair[1] in father_chrompairs:
             print("pair ", pair, "father ", father_chrompairs)
             if check == False:
@@ -54,4 +54,4 @@ for i in range(num_of_possible_fathers):
         check = not check
 
 
-print("what Maury Povich might say: ", actual_father)
+print(f"{actual_father}, you are the father!")
