924677caefa0b8c21b53ff26a2136717194c23bb
cfranklin121 <courtenayfranklin@gmail.com>
2025-04-06 21:39:30 -0700

    7 / 9 correct

--- you_are_the_father/you_are_the_father.py
+++ you_are_the_father/you_are_the_father.py
@@ -19,6 +19,7 @@ child = input()
 print(child, type(child))
 child_chrompairs = get_mother_child_chromPairs(child)
 #print("Child ChromPairs: ", child_chrompairs)
+actual_father = "NONE"
 
 num_of_possible_fathers = int(input())
 x = 0
@@ -31,32 +32,32 @@ for i in range(num_of_possible_fathers):
     a_possible_father = input()
     #print(a_possible_father, type(a_possible_father))
     father_name, father_chrompairs = get_father_info(a_possible_father)
-    #print(father_name)
-    #print(father_chrompairs)
+    print(father_name)
+    print(father_chrompairs)
 
-    pair1 = False
-    pair2 = False
+    check = True
+    a = 0
     for pair in child_chrompairs:
         #false = 1st pair. true = 2nd pair
         #print(check)
-        #print(pair[0], pair[1])
-        #print("father: ", father_chrompairs)
+        print(pair[0], pair[1])
+        print("father: ", father_chrompairs[a], father_chrompairs[a+1])
         
-        if pair[0] in mother_chrompairs:
+        if pair[0] in mother_chrompairs[a] or pair[0] in mother_chrompairs[a+1]:
             x = 1
-        else:
+        if pair[1] in mother_chrompairs[a] or pair[1] in mother_chrompairs[a+1]:
             x = 0
-        
-        if pair[x] in father_chrompairs:
-            print(f"pair{[x]} ", pair[x], father_name, father_chrompairs)
-            if check == False:
-                pair1 = True
-            else:
-                pair2 = True
+        print("compare ", pair[x])
+        if pair[x] in father_chrompairs[a] or pair[x] in father_chrompairs[a+1]:
+            print(f"Match pair{[x]} ", pair[x], father_name, father_chrompairs[a], father_chrompairs[a+1])
+            
+        else:
+            check = False
+        a += 3
 
-            if pair1 == True and pair2 == True:
-                actual_father = father_name
-        check = not check
+    if check == True:
+        actual_father = father_name
+        print(f"Current father is {actual_father}")
 
-
-print(f"{actual_father}, you are the father!")
+if actual_father != "NONE":
+    print(f"{actual_father}, you are the father!")
