4307f10e88db528f0da9cbb481694d7cde92eba8
cfranklin121 <courtenayfranklin@gmail.com>
2025-04-09 19:13:42 -0700

    Added 'NATO-Alphabet'

--- nato_alphabet/nato_alphabet.py
+++ nato_alphabet/nato_alphabet.py
@@ -1,41 +1,41 @@
 import sys
 import math
 
-def convert(word, old_alpha, new_alpha):
-    print("***IN FUNCTION***")
-    print("Word: ", word)
-    print("Old alphabet: ", old_alpha)
-    print("New alphabet: ", new_alpha)
-    if word in old_alpha:
-        print(True)
-    i = old_alpha.index(word)
-    return new_alpha[i]
-
 a_word_spelled_out = input()
+
 alpha_1 = ["Authority", "Bills", "Capture", "Destroy", "Englishmen", "Fractious", "Galloping", "High", "Invariably", "Juggling", "Knights", "Loose", "Managing", "Never", "Owners", "Play", "Queen", "Remarks", "Support", "The", "Unless", "Vindictive", "When", "Xpeditiously", "Your", "Zigzag"]
 alpha_2 = ["Apples", "Butter", "Charlie", "Duff", "Edward", "Freddy", "George", "Harry", "Ink", "Johnnie", "King", "London", "Monkey", "Nuts", "Orange", "Pudding", "Queenie", "Robert", "Sugar", "Tommy", "Uncle", "Vinegar", "Willie", "Xerxes", "Yellow", "Zebra"]
 alpha_3 = ["Amsterdam", "Baltimore", "Casablanca", "Denmark", "Edison", "Florida", "Gallipoli", "Havana", "Italia", "Jerusalem", "Kilogramme", "Liverpool", "Madagascar", "New-York", "Oslo", "Paris", "Quebec", "Roma", "Santiago", "Tripoli", "Uppsala", "Valencia", "Washington", "Xanthippe", "Yokohama", "Zurich"]
 alpha_4 = ["Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu"]
 
-print(
-    f"Word spelled out: {a_word_spelled_out}",
-      type(a_word_spelled_out),
-        file=sys.stderr, flush=True
-        )
+check_1 = True
+check_2 = True
+check_3 = True
+check_4 = True
 
 new_word = ""
 words = a_word_spelled_out.split()
-
 for word in words:
-    print("Word: ", word)
-    if word in alpha_1:
-        new_word += convert(word, alpha_1, alpha_2) + " "
-    elif word in alpha_2:
-        new_word += convert(word, alpha_2, alpha_3) + " "
-    elif word in alpha_3:
-        new_word += convert(word, alpha_3, alpha_4) + " "
-    elif word in alpha_4:
-        new_word += convert(word, alpha_4, alpha_1) + " "
+    if word not in alpha_1:
+        check_1 = False
+    if word not in alpha_2:
+        check_2 = False
+    if word not in alpha_3:
+        check_3 = False
+    if word not in alpha_4:
+        check_4 = False
 
-final = new_word.strip()
-print(final)
+if check_1:
+    for word in words:
+        new_word += alpha_2[alpha_1.index(word)] + " "
+if check_2:
+    for word in words:
+        new_word += alpha_3[alpha_2.index(word)] + " "
+if check_3:
+    for word in words:
+        new_word += alpha_4[alpha_3.index(word)] + " "
+if check_4:
+    for word in words:
+        new_word += alpha_1[alpha_4.index(word)] + " "
+
+print(new_word.strip())
