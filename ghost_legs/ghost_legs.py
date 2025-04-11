0849c0efd74eda929ec684e2cb4b4ecc451a5da9
cfranklin121 <courtenayfranklin@gmail.com>
2025-04-10 21:33:46 -0700

    Test cases 1 and 2 validate

--- ghost_legs/ghost_legs.py
+++ ghost_legs/ghost_legs.py
@@ -2,18 +2,27 @@ import sys
 import math
 
 w, h = [int(i) for i in input().split()]
-print(w)
-print(h)
+
+line = []
+
 for i in range(h):
-    line = input()
-    #print(line, type(line))
-    top_row = []
-    for a in range(0, w, 3):
-        if i == 0 or i == h:
-            print(line[a], end="")
-        else:
-            print(line[a], end="")
-    print("\n")
+    line.append(input())
+
+for i in range(h):
+      line[i] = " " + line[i] + " "
 
 
-print("answer")
+for x in range(1, w+1, 3):
+    start = line[0][x]
+    for i in range(2, h, 1):
+        
+        if line[i][x+1] == "-":
+                x += 3
+        
+        elif line[i][x-1] == "-":
+                x -= 3
+        
+    end = line[h-1][x]
+
+    print(f"{start}{end}")
+    
