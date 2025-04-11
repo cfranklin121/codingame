2233f3509c4fa3e370605aa15f326a004207d0c6
cfranklin121 <courtenayfranklin@gmail.com>
2025-04-11 16:45:54 -0700

    Minor clean-up to ghost_legs.py

--- ghost_legs/ghost_legs.py
+++ ghost_legs/ghost_legs.py
@@ -2,27 +2,17 @@ import sys
 import math
 
 w, h = [int(i) for i in input().split()]
-
 line = []
 
 for i in range(h):
     line.append(input())
-
-for i in range(h):
-      line[i] = " " + line[i] + " "
-
+    line[i] = " " + line[i] + " "   
 
 for x in range(1, w+1, 3):
-    start = line[0][x]
+    print(line[0][x], end="")
     for i in range(1, h, 1):
-        
-        if line[i][x+1] == "-":
+        if line[i][x+1] == "-":                
                 x += 3
-        
-        elif line[i][x-1] == "-":
+        elif line[i][x-1] == "-":                
                 x -= 3
-        
-    end = line[h-1][x]
-
-    print(f"{start}{end}")
-    
+    print(line[h-1][x])
