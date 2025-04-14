6e4d355354452cf8e6666f7404f0b0f8f3ba13e6
cfranklin121 <courtenayfranklin@gmail.com>
2025-04-13 18:19:09 -0700

    Multiple sand layers

--- sand_fall/sand_fall.py
+++ sand_fall/sand_fall.py
@@ -3,13 +3,14 @@ import math
 
 w, h = [int(i) for i in input().split()]
 n = int(input())
-print(f"w:{w} h:{h}")
-print(f"n:{n}")
+
+'''
 for i in range(n):
     inputs = input().split()
     s = inputs[0]
     p = int(inputs[1])
     print(f"s:{s} p:{p}")
+'''
 
 #Build the box
 box = []
@@ -33,29 +34,53 @@ for i in range(h+1):
         print()
 
 p = 2
-box[h-1][p] = "x"
+#box[h-1][p] = "x"
 #box[h-1][p+1] = "x"
-s = "A"
-x = p
-y = h
+
+y = h - 1
+full = False
 
 
-if s.isupper():
-    if box[y-1][x] == " ":
-        box[y-1][x] = s
-    elif x-1 != 0 and box[y-1][x-1] == " ":
-        box[y-1][x-1] = s
-    elif x+1 != w+1 and box[y-1][x+1] == " ":
-        box[y-1][x+1] = s
+a = 0
+for i in range(n):
+    placed = False
+    inputs = input().split()
+    s = inputs[0]
+    p = int(inputs[1])
+    x = p + 1
+    while not placed:
+        if s.isupper():
+            if box[y][x] == " ":
+                box[y][x] = s
+                placed = True
+            elif x-1 != 0 and box[y][x-1] == " ":
+                box[y][x-1] = s
+                placed = True
+            elif x+1 != w+1 and box[y][x+1] == " ":
+                box[y][x+1] = s
+                placed = True
+            else:
+                y -= 1
 
-elif s.islower():
-    if box[y-1][x] == " ":
-        box[y-1][x] = s
-    elif x+1 != w+1 and box[y-1][x+1] == " ":
-        box[y-1][x+1] = s
-    elif x-1 != 0 and box[y-1][x-1] == " ":
-        box[y-1][x-1] = s
+        elif s.islower():
+            if box[y][x] == " ":
+                box[y][x] = s
+                placed = True
+            elif x+1 != w+1 and box[y][x+1] == " ":
+                box[y][x+1] = s
+                placed = True
+            elif x-1 != 0 and box[y][x-1] == " ":
+                box[y][x-1] = s
+                placed = True
+            else:
+                y -= 1
 
+
+    for i in range(h+1):
+        for x in range(w+2):
+            print(box[i][x], end="")
+        print()
+    a += 2
 #Print box    
 for i in range(h+1):
     for x in range(w+2):
