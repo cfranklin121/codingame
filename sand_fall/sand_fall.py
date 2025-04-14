5f6ca80b4960dd8c8affdaf2b13218a09cab0c72
cfranklin121 <courtenayfranklin@gmail.com>
2025-04-13 19:51:22 -0700

    Removed multiple print lines

--- sand_fall/sand_fall.py
+++ sand_fall/sand_fall.py
@@ -4,14 +4,6 @@ import math
 w, h = [int(i) for i in input().split()]
 n = int(input())
 
-'''
-for i in range(n):
-    inputs = input().split()
-    s = inputs[0]
-    p = int(inputs[1])
-    print(f"s:{s} p:{p}")
-'''
-
 #Build the box
 box = []
 for i in range(h+1):
@@ -22,24 +14,17 @@ for i in range(h+1):
                 box[i].append("+")
             else:
                 box[i].append("-")
-            print(box[i][x], end="")
-        print()
     else:
         for x in range(w+2):
             if x == 0 or x == w+1:
                 box[i].append("|")
             else:
                 box[i].append(" ")
-            print(box[i][x], end="")
-        print()
 
 p = 2
-#box[h-1][p] = "x"
-#box[h-1][p+1] = "x"
 
 y = h - 1
 full = False
-
 
 a = 0
 for i in range(n):
@@ -75,11 +60,6 @@ for i in range(n):
             else:
                 y -= 1
 
-
-    for i in range(h+1):
-        for x in range(w+2):
-            print(box[i][x], end="")
-        print()
     a += 2
 #Print box    
 for i in range(h+1):
