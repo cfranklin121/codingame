9001a44058d11708cd90bd67727187a2ad0055e3
cfranklin121 <courtenayfranklin@gmail.com>
2026-07-20 21:12:10 -0700

    Added 'Jump the Queue'

--- jump_the_queue/main.go
+++ jump_the_queue/main.go
@@ -8,11 +8,6 @@ import (
 	"strings"
 )
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Buffer(make([]byte, 1000000), 1000000)
@@ -22,18 +17,77 @@ func main() {
 	scanner.Scan()
 	fmt.Sscan(scanner.Text(), &g, &e)
 
+	groups := []string{}
 	for i := 0; i < g; i++ {
 		scanner.Scan()
 		group := scanner.Text()
-		_ = group // to avoid unused error
+		groups = append(groups, group)
 	}
+
+	que := []int64{}
+
 	scanner.Scan()
 	inputs = strings.Split(scanner.Text(), " ")
 	for i := 0; i < e; i++ {
 		event, _ := strconv.ParseInt(inputs[i], 10, 32)
-		_ = event
+		if event > 0 {
+			result := inGroup(event, groups)
+			if result >= 0 { // Friend is in group
+				friendPos := FriendsInQue(que, groups[result])
+				if friendPos >= 0 { //Friend in line
+					que = insertToQue(que, friendPos, event)
+				} else {
+					que = append(que, event)
+				}
+
+			} else {
+				que = append(que, event)
+			}
+		} else {
+			fmt.Println(que[0])
+			que = que[1:]
+		}
 	}
 
-	// fmt.Fprintln(os.Stderr, "Debug messages...")
-	fmt.Println("answer") // Write answer to stdout
+}
+
+func inGroup(event int64, groups []string) int {
+	for j := 0; j < len(groups); j++ {
+		groupInt := strings.Split(groups[j], " ")
+
+		for k := 0; k < len(groupInt); k++ {
+			studentID, _ := strconv.ParseInt(groupInt[k], 10, 32)
+			if event == studentID {
+				return j
+			}
+		}
+	}
+
+	return -1
+}
+
+func FriendsInQue(que []int64, group string) int {
+	pos := -1
+	for i := 0; i < len(que); i++ {
+		groupInt := strings.Split(group, " ")
+		for j := 0; j < len(groupInt); j++ {
+			studentID, _ := strconv.ParseInt(groupInt[j], 10, 32)
+			if studentID == que[i] {
+				pos = i
+			}
+		}
+	}
+	return pos
+}
+
+func insertToQue(que []int64, friendPos int, friend int64) []int64 {
+	front := que[:friendPos + 1]
+	back := que[friendPos + 1:]
+
+	newQue := []int64{}
+	newQue = append(newQue, front...)
+	newQue = append(newQue, friend)
+	newQue = append(newQue, back...)
+
+	return newQue
 }
