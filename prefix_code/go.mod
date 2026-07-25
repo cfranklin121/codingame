a92a45f861cdbf5bb8b6cf7e18c3209911f11258
cfranklin121 <courtenayfranklin@gmail.com>
2026-07-25 16:53:17 -0700

    Prefix Code: initial commit

---
+++ prefix_code/go.mod
@@ -0,0 +1,3 @@
+module github.com/cfranklin121/codingame/prefix_code
+
+go 1.25.0
---
+++ prefix_code/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+func main() {
+    var n int
+    fmt.Scan(&n)
+    
+    for i := 0; i < n; i++ {
+        var b string
+        var c int
+        fmt.Scan(&b, &c)
+    }
+    var s string
+    fmt.Scan(&s)
+    
+    
+    // fmt.Fprintln(os.Stderr, "Debug messages...")
+    fmt.Println("abracadabra")// Write answer to stdout
+}
